16fea3a95ab9338387aaf5ad9d72aa7eefa64e97
songeunn <59230@naver.com>
2023-04-03 21:39:36 +0900

    feat / SEO 크롤링 최적화 - React-snap 추가

--- src/index.js
+++ src/index.js
@@ -5,12 +5,29 @@ import "../src/styles/custom.css";
 import App from "./App";
 import { Provider } from "react-redux";
 import { store } from "./redux/store";
+import { hydrate } from "react-dom";
 
+const rootElement = document.getElementById("root");
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(
-  // <React.StrictMode>
-  <Provider store={store}>
-    <App />
-  </Provider>
-  // </React.StrictMode>
-);
+
+if (rootElement.hasChildNodes()) {
+  hydrate(
+    <Provider store={store}>
+      <App />
+    </Provider>,
+    rootElement
+  );
+} else {
+  root.render(
+    <Provider store={store}>
+      <App />
+    </Provider>
+  );
+}
+// root.render(
+//   <React.StrictMode>
+//     <Provider store={store}>
+//       <App />
+//     </Provider>
+//   </React.StrictMode>
+// );
