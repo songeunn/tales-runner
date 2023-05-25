028718148ad412d0932aa3f9af2b087f9b86ac44
songeunn <59230@naver.com>
2023-05-25 20:20:50 +0900

    fix / App 컴포넌트 tools Route 추가

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import ReactGA from "react-ga";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Root from "./routes/Root/root";
 import NoticePage from "./routes/NoticePage";
+import ToolsPage from "./routes/ToolsPage";
 
 const gaTrackingId = process.env.REACT_APP_GA_TRACKING_ID; // 환경 변수에 저장된 추적ID 가져오기
 ReactGA.initialize(gaTrackingId); // react-ga 초기화 및 debug 사용 { debug: true }
@@ -13,6 +14,7 @@ function App() {
       <Routes>
         <Route path="/" element={Root} />
         <Route path="/notice" element={NoticePage} />
+        <Route path="/tools" element={ToolsPage} />
       </Routes>
     </BrowserRouter>
   );
